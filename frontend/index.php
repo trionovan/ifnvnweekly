f8dc4f6e120e002d95c2968112523a63ff250938
nvnn <trio.clasher2@gmail.com>
2026-07-05 22:49:06 +0700

    add homepage

--- frontend/index.php
+++ frontend/index.php
@@ -4,41 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Informatics</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>INFORMATIKA 2026</h1>
-    <table border = "1" cellspacing = "0" cellpadding = "10">
-        <tr>
-            <th><a href="index.php">Home</a></th>
-            <th><a href="profile.php">Profile</a></th>
-            <th><a href="contact.php">Contact</a></th>
-            <th><a href="mahasiswa.php">Mahasiswa</a></th>
-        </tr>
-    </table>
-    <br>
-    <hr/>
+
+    <header class="site-header">
+        <h1>INFORMATIKA <span>2026</span></h1>
+
+        <table border="1" cellspacing="0" cellpadding="10">
+            <tr>
+                <th><a href="index.php">Home</a></th>
+                <th><a href="profile.php">Profile</a></th>
+                <th><a href="contact.php">Contact</a></th>
+                <th><a href="mahasiswa.php">Mahasiswa</a></th>
+            </tr>
+        </table>
+    </header>
+
     <hr>
+
     <h3>Sambutan Ketua</h3>
-    <img src="../assets/images/kaprodi.jpg" width="200px" >
-    <p>
-        <b>
-        Nama : Andi <br>
-        Jabatan : Student of Colleague <br>
-        </b>
-        <i>Deskripsi : Lorem ipsum dolor sit amet, 
-        consectetur adipisicing elit. 
-        Enim omnis praesentium quo maxime tenetur 
-        aliquid magni numquam <u><b>iusto ipsam,</b></u> in atque 
-        delectus quae. Minima facilis nam eos, fugiat 
-        quasi voluptates? <b>H<sub>2</sub>O 2<sup>2</sup></b></i>
-    </p>
 
-    <a href="profile.php" >profile</a><br>
-    <a href="contact.php">contact</a><br>
-    <a href="https://youtube.com" target="_blank">Youtube</a>
+    <div class="terminal-card">
+        <div class="terminal-topbar">
+            <span class="dot red"></span>
+            <span class="dot yellow"></span>
+            <span class="dot green"></span>
+            <span class="terminal-filename">sambutan_ketua.php</span>
+        </div>
+        <div class="terminal-body">
+            <img src="../assets/Image/kaprodi.jpg" alt="Foto Ketua">
+            <div class="bio">
+                <p><b>Nama :</b> Andi</p>
+                <p><b>Jabatan :</b> Student of Colleague</p>
+                <i>Deskripsi : Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Enim omnis praesentium quo maxime tenetur aliquid magni numquam
+                <u><b>iusto ipsam,</b></u> in atque delectus quae. Minima facilis nam eos,
+                fugiat quasi voluptates? <b>H<sub>2</sub>O 2<sup>2</sup></b></i>
+            </div>
+        </div>
+    </div>
 
-    <link rel="stylesheet" href="style.css">
-    
+    <div class="btn-group">
+        <a href="profile.php">Profile</a>
+        <a href="contact.php">Contact</a>
+        <a href="https://youtube.com" target="_blank">Youtube</a>
+    </div>
+
+    <footer class="site-footer">
+        © 2026 Informatika UNIMUS — dibuat dengan PHP & MySQL
+    </footer>
+
 </body>
 </html>
